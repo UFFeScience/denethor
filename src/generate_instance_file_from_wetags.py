8b19211489f283ded85c6b4ac3a47e3c1f7c82c8
Marcello Ribeiro <marcellowmr@gmail.com>
2025-04-18 18:11:42 -0300

    Add execution tracking and wetag handling in instance generation script; refactor SQL execution functions for improved clarity and performance.

--- src/generate_instance_file_from_wetags.py
+++ src/generate_instance_file_from_wetags.py
@@ -2,104 +2,70 @@ import os, re
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 from denethor.core.database.conn import Connection
-from instance_generator import utils as igu
 from datetime import datetime
 
-#config basica: 128 + 256 + 512 + 128
-# WEID = 'weid_1724184708846' // weid_1730480017077  #002
-# WEID = 'weid_1724428668735' // weid_1730479857730  #005
-# WEID = 'weid_1724433692051' #010
-# WEID = 'weid_1722560049311' #050
-
-# config: 256 + 512 + 1024 + 128
-# WEID = 'weid_1730474091468' #002
-# WEID = 'weid_1729872467000' #005
-
-# 002 / config n1 + n2
-# WEID = ['weid_1730480017077', 'weid_1730474091468']
-
-# 005 / config n1 + n2
-# WEID = ['weid_1730479857730', 'weid_1729872467000']
-
-# 2 files with:
-
-#       128mb
-# WEID = ['weid_1735311414938']
-
-#       128mb               / 256mb               / 512                  / 1024               / 2048 execution
-#WEID = ['weid_1735311414938', 'weid_1735318446694', 'weid_1735319031426', 'weid_1735319100179', 'weid_1735319150437']
-
-
-# execução em função lambda com 2 entradas e todas as configurações
-# execução em vm: wetag_1743630589148
-#WETAG = ['wetag_1743638228939', 'wetag_1743638805975', 'wetag_1743638866699', 'wetag_1743638915763', 'wetag_1743639013205']
-
-
-# execução em função lambda com 5 entradas e todas as configurações
-# execução em vm: 
-#WETAG = ['wetag_1743678423945', 'wetag_1743678525527', 'wetag_1743678605279', 'wetag_1743678680757', 'wetag_1743678753528']
-
-# execução em função lambda com 10 entradas e todas as configurações
-WETAG = ['wetag_1743784949015', 'wetag_1743784751635', 'wetag_1743784544684', 'wetag_1743784315852', 'wetag_1743783985655']
-# execução em vm: wetag_1743787322379
-
-SQL_FILES_PATH = 'resources/sql/instance_generator/'  # Diretório onde os arquivos SQL estão localizados
-
+SQL_FILES_PATH = 'scripts/sql/instance_generator/'  # Diretório onde os arquivos SQL estão localizados
 INSTANCE_FILE_PATH = 'resources/data/instance_files/'  # Diretório onde os arquivos de instância serão salvos
-
 WRITE_COMMENTS_TO_FILE = True
+
+# Connect to the PostgreSQL database
+session = Connection().get_session()
+
+
+
+# execução em função lambda com 50 entradas e todas as configurações
+# execução em vm: xxxx
+WETAG_FX = ['wetag_1744757716148', 'wetag_1744767700184', 'wetag_1744771889401', 'xxx', 'xxx']
+# WETAG_VM = ['xxxx']
 
 
 
 def main():
     
-    print(f"Generating model file for execution_tag: {WETAG}")
+    print(f"Generating model file for execution_tag: {WETAG_FX}")
 
-    # Conecte-se ao banco de dados PostgreSQL
-    connection = Connection()
-    session = connection.get_session()
-
-    execution_tag = ','.join(f"'{tag}'" for tag in WETAG)  # Adiciona aspas simples ao redor de cada elemento
+    execution_tag_fx = ','.join(f"'{tag}'" for tag in WETAG_FX)  # Adiciona aspas simples ao redor de cada elemento
 
     # execute sql instance count
-    count = igu.execute_sql_instance_count(execution_tag, session)
-    out_file_name = f'model_instance_{count}_{execution_tag}.txt'.replace(',', '__').replace("'", '')
+    count = execute_sql_input_count(execution_tag_fx)
+    out_file_name = f'model_instance_{count}_{execution_tag_fx}.txt'.replace(',', '__').replace("'", '')
     
-    out_file = os.path.join(INSTANCE_FILE_PATH, out_file_name)
+    out_file_data = os.path.join(INSTANCE_FILE_PATH, out_file_name)
 
-    if os.path.exists(out_file):
-        os.remove(out_file)
+    if os.path.exists(out_file_data):
+        os.remove(out_file_data)
     
-    output_sqls_file = out_file.replace('.txt', '_executed.sql')
-    if os.path.exists(output_sqls_file):
-        os.remove(output_sqls_file)
+    out_file_sqls = out_file_data.replace('.txt', '_executed.sql')
+    if os.path.exists(out_file_sqls):
+        os.remove(out_file_sqls)
 
-    # Listar todos os arquivos .sql no diretório especificado e ordenar por nome
+    # List all .sql files in the specified directory and sort them by name
     sql_files = sorted([f for f in os.listdir(SQL_FILES_PATH) if f.endswith('.sql')])
 
     for sql_file_name in sql_files:
         
-        print(f"Executing {sql_file_name} with execution_tag: {execution_tag}")
+        print(f"Executing {sql_file_name} with execution_tag: {execution_tag_fx}")
         sql_file = os.path.join(SQL_FILES_PATH, sql_file_name)
 
-        execute_sql_and_save_results(execution_tag, sql_file, out_file, output_sqls_file, WRITE_COMMENTS_TO_FILE, session)
+        execute_sql_and_save_results(execution_tag_fx, sql_file, out_file_data, out_file_sqls, WRITE_COMMENTS_TO_FILE)
 
+    print(f"Model file generated: {out_file_data}")
 
 
 # Execute SQL script and save results to a file
-def execute_sql_and_save_results(execution_tag, input_sql_file, output_results_file, output_sqls_file, write_sql_comments, db):
+def execute_sql_and_save_results(execution_tag, input_sql_file, output_results_file, output_sqls_file, write_sql_comments):
 
     with open(input_sql_file, 'r') as file:
         sql_to_execute = file.read()
     
     # separate comments and code
-    comments, sql_to_execute = igu.separate_comments_and_code(sql_to_execute)
+    comments, sql_to_execute = separate_comments_and_code(sql_to_execute)
 
     # replace wetag
-    sql_to_execute = igu.replace_wetag(execution_tag, sql_to_execute)
+    sql_to_execute = replace_wetag(execution_tag, sql_to_execute)
     
     try:
-        result = db.execute(text(sql_to_execute))
+        result = session.execute(text(sql_to_execute))
         results = result.fetchall()
         
         with open(output_results_file, 'a') as file:
@@ -123,7 +89,64 @@ def execute_sql_and_save_results(execution_tag, input_sql_file, output_results_f
         print(f"Error executing {input_sql_file}: {e}")
     
     finally:
-        db.close()
+        session.close()
+
+
+# Remove comments from SQL script
+def remove_comments(sql):
+    lines = sql.split('\n')
+    filtered_lines = [line for line in lines if not line.strip().startswith('--')]
+    return '\n'.join(filtered_lines)
+
+
+# This function separates comments (starting with '--#') from the SQL code
+# and returns them as two separate strings.
+def separate_comments_and_code(sql: str):
+    lines = sql.split('\n')
+    comments = []
+    code = []
+    
+    for line in lines:
+        if line.strip().startswith('--#'):
+            comments.append(line.strip().replace('--',''))
+        else:
+            code.append(line)
+    
+    comments_str = '\n'.join(comments)
+    code_str = '\n'.join(code)
+    
+    return comments_str, code_str
+
+
+# Find all occurrences of wetag_\d+ or '[wetag]' in the SQL script
+def replace_wetag(execution_tag, sql):
+    pattern = r'wetag_\d+|\[wetag\]'
+    wetag_occurrences = list(set(re.findall(pattern, sql)))
+    
+    for occurrence in wetag_occurrences:
+        sql = sql.replace(occurrence, execution_tag)
+        sql = sql.replace("''", "'")
+    return sql
+
+
+# Execute SQL to get the input count for the given execution tag
+def execute_sql_input_count(execution_tag: list):
+    SQL_INPUT_COUNT = f"\
+        SELECT max(input_count) \
+        FROM workflow_execution we \
+        WHERE we.execution_tag in ({execution_tag})"
+    
+    print(f"Executing SQL: {SQL_INPUT_COUNT}")
+
+    try:
+        result = session.execute(text(SQL_INPUT_COUNT))
+        return result.scalar()
+    except SQLAlchemyError as e:
+        print(f"Error executing instance count SQL: {e}")
+        return None
+    finally:
+        session.close()
+
 
 
 if __name__ == "__main__":
