2b1b629a029351fe0c073d5e71d4dc07bf44d77d
Marcello Ribeiro <marcellowmr@gmail.com>
2025-04-18 18:09:57 -0300

    Enhance workflow execution with dynamic file count and memory settings; add logging for execution details and metadata storage.

--- src/run_workflow__local__on_ec2_vm.py
+++ src/run_workflow__local__on_ec2_vm.py
@@ -1,4 +1,5 @@
 import os, time, json
+import sys
 from pathlib import Path
 from denethor.utils import utils as du, file_utils as dfu
 from denethor.executor import execution_manager as dem
@@ -30,24 +31,33 @@ with open(os.path.join(conf_path, "statistics.json"), "r") as f:
 # Load the environment configuration
 env_properties = du.load_properties(os.path.join(conf_path, "env.properties"))
 
+# List of files to be processed
+input_dir = os.path.join(project_root, "resources/data/full_dataset")
+
 # Dictionary to store the produced data during the workflow execution
 runtime_data = {}
 
 
 def main():
-
     print(">>> Main program started at: ", du.now_str())
-
 
     #################################################################
     #
     # !!!! Force execution parameters !!!!
     #
     #################################################################
-    FORCE_ENV = const.AWS_EC2
-    FORCE_MEMORY = None
+    FORCE_PROVIDER = const.AWS_EC2
+    FORCE_MEMORY = 1024
 
-    override_params(FORCE_ENV, FORCE_MEMORY)
+    INPUT_FILE_LIST = None
+
+    file_count = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    # file_count = 50
+
+    INPUT_FILE_LIST = dfu.list_first_n_files(input_dir, file_count)
+
+    print(f"Using {file_count} files: {INPUT_FILE_LIST}")
+    du.override_params(workflow_steps, FORCE_PROVIDER, FORCE_MEMORY, INPUT_FILE_LIST)
 
     #################################################################
 
@@ -56,35 +66,52 @@ def main():
     ##
     execution_tag, workflow_start_time_ms, workflow_end_time_ms, runtime_data = (
         dexec.execute_workflow(
-        workflow_steps,
-        env_properties,
+            workflow_steps,
+            env_properties,
         )
     )
 
+    metadata_file = (
+        env_properties.get("denethor")
+        .get("log.metadata_file")
+        .replace("[execution_tag]", execution_tag)
+    )
 
+    log_path = (
+        env_properties.get("denethor")
+        .get("log.path")
+        .replace("[provider_tag]", FORCE_PROVIDER)
+    )
 
+    # Write execution details to log_metadata in JSON format
+    metadata_content = {
+        "date_time_utc": du.now_str(),
+        "workflow_name": workflow_info.get("workflow_name"),
+        "execution_tag": execution_tag,
+        "workflow_start_time_ms": workflow_start_time_ms,
+        "workflow_end_time_ms": workflow_end_time_ms,
+        "runtime_data": runtime_data,
+        "workflow_steps": workflow_steps,
+        "env_properties": env_properties,
+    }
 
-        ##
-        ## Import provenance data
-        ##
-        # dprov.import_provenance_from_aws(
-        #     execution_info,
-        #     workflow_info,
-        #     workflow_steps,
-        #     statistics_info,
-        #     env_properties,
-        # )
+    with open(os.path.join(log_path, metadata_file), "w") as mf:
+        json.dump(metadata_content, mf, indent=4)
 
-
-
-def override_params(env=None, memory=None):
-    for step in workflow_steps:
-        if env:
-            step['provider'] = env
-            print(f"Warning: Overriding environment to {env}!")
-        if memory:
-            step['memory'] = memory
-            print(f"Warning: Overriding memory size of step {step['activity']} to {memory}MB!")
+    ##
+    ## Import provenance data
+    ##
+    # dprov.import_provenance_from_aws(
+    #         execution_tag,
+    #         workflow_start_time_ms,
+    #         workflow_end_time_ms,
+    #         runtime_data,
+    #         provider_info,
+    #         workflow_info,
+    #         workflow_steps,
+    #         statistics_info,
+    #         env_properties,
+    #     )
 
 
 if __name__ == "__main__":
--- src/run_workflow_on_lambda_and_import_provenace.py
+++ src/run_workflow_on_lambda_and_import_provenace.py
@@ -1,4 +1,4 @@
-import os, time, json
+import os, sys, timeit, json
 from pathlib import Path
 from denethor import constants as denv
 from denethor.core.service import *
@@ -31,6 +31,10 @@ with open(os.path.join(conf_path, "statistics.json"), "r") as f:
 env_properties = du.load_properties(os.path.join(conf_path, "env.properties"))
 
 
+# List of files to be processed
+input_dir = os.path.join(project_root, "resources/data/full_dataset")
+
+
 def main():
 
     print(">>> Main program started at: ", du.now_str())
@@ -40,53 +44,87 @@ def main():
     # !!!! Force execution parameters !!!!
     #
     #################################################################
-    FORCE_ENV = None
+    PROVIDER = const.AWS_LAMBDA
     FORCE_MEMORY = [128, 256, 512, 1024, 2048]
+    # FORCE_MEMORY = [128]
+    # SET_ACTIVE_STEPS = ["tree_constructor", "subtree_constructor"]
+    SET_ACTIVE_STEPS = None #dont override active steps
+    INPUT_FILE_LIST = None
+    # FILE_COUNT_LIST = [2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    FILE_COUNT_LIST = [15, 20, 25, 30, 35, 40, 45, 50]
+
+    # file_count = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    # file_count = 2
+
 
     #################################################################
 
-    for memory in FORCE_MEMORY:
+    for N_FILES in FILE_COUNT_LIST:
+        print(f"Using {N_FILES} files: {INPUT_FILE_LIST}")
 
-        override_params(FORCE_ENV, memory)
+        INPUT_FILE_LIST = dfu.list_first_n_files(input_dir, N_FILES)
+        
+        for MEMORY in FORCE_MEMORY:
 
-        ##
-        ## Execute the workflow
-        ##
-        execution_tag, workflow_start_time_ms, workflow_end_time_ms, runtime_data = (
-            dexec.execute_workflow(
+            du.override_params(
+                workflow_steps, PROVIDER, MEMORY, INPUT_FILE_LIST, SET_ACTIVE_STEPS
+            )
+
+            ##
+            ## Execute the workflow
+            ##
+            execution_tag, workflow_start_time_ms, workflow_end_time_ms, runtime_data = (
+                dexec.execute_workflow(
+                    workflow_steps,
+                    env_properties,
+                )
+            )
+
+            metadata_file = (
+                env_properties.get("denethor")
+                .get("log.metadata_file")
+                .replace("[execution_tag]", execution_tag)
+                .replace("[n_files]", f"{N_FILES:03}")
+                .replace("[memory]", f"{MEMORY:04}")
+            )
+
+            log_path = (
+                env_properties.get("denethor")
+                .get("log.path")
+                .replace("[provider_tag]", PROVIDER)
+            )
+
+            # Write execution details to log_metadata in JSON format
+            metadata_content = {
+                "date_time_utc": du.now_str(),
+                "workflow_name": workflow_info.get("workflow_name"),
+                "execution_tag": execution_tag,
+                "workflow_start_time_ms": workflow_start_time_ms,
+                "workflow_end_time_ms": workflow_end_time_ms,
+                "runtime_data": runtime_data,
+                "workflow_steps": workflow_steps,
+                "env_properties": env_properties,
+            }
+
+            with open(os.path.join(log_path, metadata_file), "w") as mf:
+                json.dump(metadata_content, mf, indent=4)
+
+            #
+            # Import provenance data
+            #
+            dprov.import_provenance_from_aws(
+                execution_tag,
+                workflow_start_time_ms,
+                workflow_end_time_ms,
+                runtime_data,
+                provider_info,
+                workflow_info,
                 workflow_steps,
+                statistics_info,
                 env_properties,
             )
-        )
 
-        ##
-        ## Import provenance data
-        ##
-        dprov.import_provenance_from_aws(
-            execution_tag,
-            workflow_start_time_ms,
-            workflow_end_time_ms,
-            runtime_data,
-            provider_info,
-            workflow_info,
-            workflow_steps,
-            statistics_info,
-            env_properties,
-        )
-
-    print(">>> Main program finished at: ", du.now_str())
-
-
-def override_params(env=None, memory=None):
-    for step in workflow_steps:
-        if env:
-            step["provider"] = env
-            print(f"Warning: Overriding environment to {env}!")
-        if memory:
-            step["memory"] = memory
-            print(
-                f"Warning: Overriding memory size of step {step['activity']} to {memory}MB!"
-            )
+        print(">>> Main program finished at: ", du.now_str())
 
 
 if __name__ == "__main__":
